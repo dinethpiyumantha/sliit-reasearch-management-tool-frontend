abcc133a935d18984c1f69f289cdc325a369a81a
dinethpiyumantha <dinethpiyumantha@gmail.com>
2022-06-01 07:57:53 +0530

    sidebar comp implemented

---
+++ src/components/side-bar/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div className="col-md-4">
+    <p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nisi, dolor soluta ducimus ullam quidem possimus numquam magni? Eligendi ex inventore qui, maiores asperiores voluptates ratione, id excepturi dolorem sit nostrum sapiente reprehenderit, odio deleniti molestiae quo commodi minima voluptate soluta obcaecati! Velit quia, quo nulla vero quam doloribus maxime numquam at quae unde.
+</p>
+    </div>
+  )
+}
