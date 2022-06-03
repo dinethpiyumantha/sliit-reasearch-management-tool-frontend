3763c078abb922d0526d4df17dc91904e0c25aaf
sandanis1207 <sandanis@treinetic.com>
2022-06-03 09:15:12 +0530

    Submission Types form is created

--- src/pages/admin/Submission.jsx
+++ src/pages/admin/Submission.jsx
@@ -8,8 +8,37 @@ export default function ViewRoles() {
     return (
         <div>
             <AdminNavigationBar />
+            <div className="container mt-5">
+            <div className="row d-flex justify-content-center">
+            <form class="row g-3">
+            <div class="col-md-2">
+                <label for="inputZip" class="form-label">Submission Type</label>
+                <input type="text" class="form-control" id="inputZip"></input>
+            </div>
+            <div class="col-md-6">
+                <label for="inputCity" class="form-label">Description</label>
+                <input type="text" class="form-control" id="inputCity"></input>
+            </div>
+            <div class="col-md-2">
+                <label for="inputState" class="form-label">File Type</label>
+                <select id="inputState" class="form-select">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+            </div>
+            <div class="col-md-2">
+                <label for="inputZip" class="form-label">Deadline</label>
+                <input type="text" class="form-control" id="inputZip"></input>
+            </div>
+            <div class="d-grid gap-2 d-md-block">
+                <button class="btn btn-primary" type="button">Add Items</button>
+                <button class="btn btn-primary" type="button">Submit</button>
+            </div>
+            </form>
             
             <FooterBar />
+            </div>
+            </div>
         </div>
     )
 }
